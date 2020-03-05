52993a82e339a1352ed4941562adc836366c1387
Ol Gen <oggen18@gmail.com>
2020-03-05 21:19:15 +0300

    0. Инструменты[#242388]3

--- chapter_001/src/main/java/ru/job4j/kiss/MaxMin.java
+++ chapter_001/src/main/java/ru/job4j/kiss/MaxMin.java
@@ -4,12 +4,12 @@ import java.util.Comparator;
 import java.util.List;
 
 public class MaxMin {
-    public <T> T max(List<T> value, Comparator <T> comparator) {
+    public <T> T max(List<T> value, Comparator<T> comparator) {
         value.sort(comparator);
         return value.get(value.size() - 1);
     }
 
-    public <T> T min(List<T> value, Comparator <T> comparator) {
+    public <T> T min(List<T> value, Comparator<T> comparator) {
         return max(value, comparator);
     }
 }
