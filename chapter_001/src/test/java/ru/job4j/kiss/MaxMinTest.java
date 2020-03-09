a60c5bfefc6025ec65429fd4b77d4956a3ea9ad0
Ol Gen <oggen18@gmail.com>
2020-03-09 15:06:59 +0300

    1. Принципы Kiss, Dry и Yagni[#242387]Test

---
+++ chapter_001/src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.kiss;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class MaxMinTest {
+    @Test
+    public void whenGetMaxElement() {
+        MaxMin maxMin = new MaxMin();
+        List<Integer> list = List.of(2, 2, 3, 4, 9, 5, 1, 7, 8);
+        Integer expected = 9;
+        Integer result = maxMin.max(list, (el1, el2) -> el1.compareTo(el2));
+        assertThat(result, is(expected));
+    }
+    @Test
+    public void whenGetMinElement() {
+        MaxMin maxMin = new MaxMin();
+        List<Integer> list = List.of(2, 2, 3, 4, 9, 5, 1, 7, 8);
+        Integer expected = 1;
+        Integer result = maxMin.max(list, (el1, el2) -> el2.compareTo(el1));
+        assertThat(result, is(expected));
+    }
+}
