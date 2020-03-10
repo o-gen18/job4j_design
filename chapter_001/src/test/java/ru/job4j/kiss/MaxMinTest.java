d6ec1f898e0accd9c4f91d95ff1ef885ece1c18e
Ol Gen <oggen18@gmail.com>
2020-03-10 22:19:30 +0300

    1. Принципы Kiss, Dry и Yagni[#242387]final

--- chapter_001/src/test/java/ru/job4j/kiss/MaxMinTest.java
+++ chapter_001/src/test/java/ru/job4j/kiss/MaxMinTest.java
@@ -4,8 +4,8 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.junit.Assert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MaxMinTest {
     @Test
