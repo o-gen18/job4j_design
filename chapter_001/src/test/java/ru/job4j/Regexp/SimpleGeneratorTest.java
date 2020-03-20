373719ace09b47b01a2691f5ac929a9213857355
Ol Gen <oggen18@gmail.com>
2020-03-20 15:42:32 +0300

    3. Исправить код класс SimpleGenerator.[#242385] тест

---
+++ chapter_001/src/test/java/ru/job4j/Regexp/SimpleGeneratorTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.Regexp;
+
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class SimpleGeneratorTest {
+    @Test
+    public void whenPassNameAndSubjectThenReplaces() {
+        SimpleGenerator simpleGen = new SimpleGenerator();
+        Map<String, String> keys = Map.of("name", "Petr", "subject", "you");
+        String template = "I am ${name}, Who are ${subject}?";
+        String expected = "I am Petr, Who are you?";
+        String result = simpleGen.generate(template, keys);
+        assertThat(result, is(expected));
+    }
+}
