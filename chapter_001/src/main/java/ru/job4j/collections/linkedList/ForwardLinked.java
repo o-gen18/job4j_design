c05e628f377afabb1451fdb3e133a115ab009e63
Ol Gen <oggen18@gmail.com>
2020-05-03 18:36:23 +0300

    5.3.0 Создать метод delete для односвязного списка[#242313]

---
+++ chapter_001/src/main/java/ru/job4j/collections/linkedList/ForwardLinked.java
@@ -0,0 +1,59 @@
+package ru.job4j.collections.linkedList;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ForwardLinked<T> implements Iterable<T> {
+    private Node<T> head;
+
+    private static class Node<T> {
+        T value;
+        Node<T> next;
+
+        public Node(T value, Node<T> next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    public void add(T value) {
+        Node<T> node = new Node<T>(value, null);
+        if (head == null) {
+            head = node;
+            return;
+        }
+        Node<T> tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    }
+
+    public void deleteFirst() {
+        if (head != null) {
+            head = head.next;
+        } else throw new NoSuchElementException();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Node<T> node = head;
+
+            @Override
+            public boolean hasNext() {
+                return node != null;
+            }
+
+            @Override
+            public T next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                T value = node.value;
+                node = node.next;
+                return value;
+            }
+        };
+    }
+}
