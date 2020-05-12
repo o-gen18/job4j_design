60efe8bf996663b9885ff158e41c1bf4b88ff607
Ol Gen <oggen18@gmail.com>
2020-05-12 22:54:46 +0300

    1. Создать модель User[#242333]

---
+++ chapter_001/src/main/java/ru/job4j/collections/map/SimpleMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collections.map;
+
+import java.util.Calendar;
+
+public class SimpleMap {
+    private final static class User {
+        private String name;
+        private int children;
+        private Calendar birthday;
+
+        public User(String name, int children, Calendar birthday) {
+            this.name = name;
+            this.children = children;
+            this.birthday = birthday;
+        }
+    }
+}
