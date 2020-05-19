3780cf663eb00af3317a49ed045b0a4c36e98666
Ol Gen <oggen18@gmail.com>
2020-05-19 12:07:45 +0300

    8. Реализовать собственную структуру данных - HashMap[#242340] 2

--- chapter_001/src/main/java/ru/job4j/collections/map/MyHashMap.java
+++ chapter_001/src/main/java/ru/job4j/collections/map/MyHashMap.java
@@ -3,46 +3,105 @@ package ru.job4j.collections.map;
 import java.util.*;
 
 public class MyHashMap<K, V> implements Iterable<V> {
-    private Object[] array;
+    private Entry<K, V>[] array;
 
     private int index = 0;
 
     private int modCount = 0;
 
+    private final float LOAD_FACTOR = 0.75f;
+
     private int latestCapacity;
 
+    private static class Entry<K, V> {
+        final int hash;
+        final K key;
+        V value;
+        Entry<K, V> next;
+
+        protected Entry(int hash, K key, V value, Entry<K, V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public V setValue(V value) {
+            if (value == null) {
+                throw new NullPointerException();
+            }
+            V oldValue = this.value;
+            this.value = value;
+            return oldValue;
+        }
+    }
+
+    private int rehash(K key, Entry<?, ?>[] newArray) {
+        int hash = Objects.hash(key);
+        return (hash & 0x7FFFFFFF) % newArray.length;
+    }
 
     private void enlargeArray() {
-        array = Arrays.copyOf(array, latestCapacity * 2);
+        Entry<K, V>[] newArray = (Entry<K, V>[]) new Entry<?, ?>[latestCapacity << 1];
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] == null) {
+                continue;
+            }
+            for (Entry<K, V> pair = (Entry<K, V>) array[i]; pair != null; ) {
+                int position = rehash(pair.getKey(), newArray);
+                if (newArray[position] != null) {
+                    newArray[position].next = pair;
+                } else {
+                    newArray[position] = pair;
+                }
+                pair = pair.next;
+            }
+        }
+        array = newArray;
         latestCapacity = array.length;
-        modCount++;
     }
 
     public MyHashMap() {
-        array = new Object[10];
+        array = (Entry<K, V>[]) new Entry<?, ?>[10];
         latestCapacity = array.length;
     }
 
     public boolean insert(K key, V value) {
-        if (index >= array.length) {
+        if ((float) array.length/index >= LOAD_FACTOR) {
             enlargeArray();
         }
         int hash = Objects.hash(key);
         int position = (hash & 0x7FFFFFFF) % array.length;
         if (array[position] != null) {
-            return false;
+            array[position].next = new Entry<K, V>(hash, key, value, null);
         } else {
-            array[position] = value;
+            array[position] = new Entry<>(hash, key, value, null);
             index++;
             modCount++;
-            return true;
         }
+        return true;
     }
 
     public V get(K key) {
         int hash = Objects.hash(key);
-        int index = (hash & 0x7FFFFFFF) % array.length;
-        return (V) array[index];
+        int index = (hash & 0x7FFFFFFF)  % array.length;
+        V value = null;
+        for (Entry<K, V> pair = array[index]; pair != null;) {
+            if (pair.getKey().equals(key)) {
+                value = pair.getValue();
+                break;
+            }
+            pair = pair.next;
+        }
+        return value;
     }
 
     public boolean delete(K key) {
@@ -51,7 +110,13 @@ public class MyHashMap<K, V> implements Iterable<V> {
         if (array[position] == null) {
             return false;
         } else {
-            array[position] = null;
+            for (Entry<K, V> pair = array[position]; pair != null;) {
+                if (pair.getKey().equals(key)) {
+                    pair = null;
+                    break;
+                }
+                pair = pair.next;
+            }
             index--;
             modCount++;
             return true;
@@ -61,7 +126,7 @@ public class MyHashMap<K, V> implements Iterable<V> {
     @Override
     public Iterator<V> iterator() {
         return new Iterator<V>() {
-            private Object[] arrayItr = array;
+            private Entry<K, V>[] arrayItr = array;
             private int indexItr = 0;
             private int expectedModCount = modCount;
 
@@ -86,7 +151,7 @@ public class MyHashMap<K, V> implements Iterable<V> {
                 if (expectedModCount != modCount) {
                     throw new ConcurrentModificationException();
                 }
-                return (V) arrayItr[indexItr++];
+                return (V) arrayItr[indexItr++].getValue();
             }
         };
     }
