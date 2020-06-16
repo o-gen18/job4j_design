35bd5855a36372ed9c7135d8bb94d58fd66e2485
Ol Gen <oggen18@gmail.com>
2020-06-16 18:57:32 +0300

    1. Бот[7921#242362]

--- chapter_002/src/main/java/ru/job4j/io/socket/EchoServer.java
+++ chapter_002/src/main/java/ru/job4j/io/socket/EchoServer.java
@@ -18,13 +18,19 @@ public class EchoServer {
                      BufferedReader in = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     String str;
+                    String answer = "";
                     while (!(str = in.readLine()).isEmpty()) {
                         System.out.println(str);
-                        if (str.contains("Bye")) {
+                        if (str.contains("Exit")) {
                             run = false;
                         }
+                        if (answer.equals("")) {
+                            answer = str.substring(
+                                    str.indexOf("=") + 1, str.indexOf("HTTP") - 1);
+                        }
                     }
-                    out.write("HTTP/1.1 200 OK\r\n\\".getBytes());
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(answer.getBytes());
                 }
             }
         }
