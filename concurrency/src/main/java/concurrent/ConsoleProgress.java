2b3a4d9014223adf59a0fd29494f760ad9b97811
Ol Gen <oggen18@gmail.com>
2020-10-21 20:43:05 +0300

    thread interrupting

---
+++ concurrency/src/main/java/concurrent/ConsoleProgress.java
@@ -0,0 +1,26 @@
+package concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        char[] process = {'\\', '|', '/', '-', '\\', '|', '/', '-'};
+        int i = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\rLoading..." + process[i++]);
+            i = i > 7 ? 0 : i;
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1000);
+        progress.interrupt();
+    }
+}
