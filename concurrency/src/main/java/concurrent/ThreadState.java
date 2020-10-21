cd712f491c82ce625c2fac8e8b402e784178645e
Ol Gen <oggen18@gmail.com>
2020-10-21 16:56:27 +0300

    Thread's state

---
+++ concurrency/src/main/java/concurrent/ThreadState.java
@@ -0,0 +1,22 @@
+package concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> { }
+        );
+        Thread second = new Thread(
+                () -> { }
+        );
+        System.out.println(first.getName());
+        System.out.println(second.getName());
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED
+                && second.getState() != Thread.State.TERMINATED) {
+            System.out.println(first.getName() + " " + first.getState());
+            System.out.println(second.getName() + " " + second.getState());
+        }
+        System.out.println("Finished");
+    }
+}
