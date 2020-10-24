346f077649b61ffd30aa194c1eac0277bc049d14
Ol Gen <oggen18@gmail.com>
2020-10-24 22:32:34 +0300

    Limited Downloading

---
+++ concurrency/src/main/java/net/WgetDownload.java
@@ -0,0 +1,42 @@
+package net;
+
+import java.io.*;
+import java.net.URL;
+
+public class WgetDownload {
+
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalStateException(
+                    "Please, specify the url and the preferred downloading speed");
+        }
+
+        int speed = Integer.parseInt(args[1]) * 1000;
+
+        try (BufferedInputStream in = new BufferedInputStream(new URL(args[0]).openStream());
+             FileOutputStream out = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead = 0;
+
+            while (bytesRead != -1) {
+                long start = System.currentTimeMillis();
+                bytesRead = in.read(dataBuffer, 0, 1024);
+                long finish = System.currentTimeMillis();
+
+                if (bytesRead <= 0) {
+                    break;
+                }
+
+                double actualTime = finish - start;
+                double expectedTime = (bytesRead / speed) * 1000;
+                out.write(dataBuffer, 0, bytesRead);
+
+                if (actualTime < expectedTime) {
+                    Thread.sleep((long) (expectedTime - actualTime));
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
