0a25f789c616aae962996fb38bb33075140f310d
Ol Gen <oggen18@gmail.com>
2020-10-27 16:35:52 +0300

    Atomicity

---
+++ concurrency/src/main/java/atomicity/Cache.java
@@ -0,0 +1,12 @@
+package atomicity;
+
+public class Cache {
+    private static Cache cache;
+
+    public synchronized static Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
