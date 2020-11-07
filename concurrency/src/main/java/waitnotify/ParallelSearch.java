108f67ab3203554a41477981a5528e60e63c5644
Ol Gen <oggen18@gmail.com>
2020-11-07 22:31:37 +0300

    ParallelSearch

---
+++ concurrency/src/main/java/waitnotify/ParallelSearch.java
@@ -0,0 +1,29 @@
+package waitnotify;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        System.out.println(queue.poll());
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+        ).start();
+    }
+}
