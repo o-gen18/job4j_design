b0e0742921b8b7d978225fc58c09020e2ca0e7ed
Ol Gen <oggen18@gmail.com>
2020-11-08 15:02:20 +0300

    SimpleBlockingQueueTest

---
+++ concurrency/src/test/java/waitnotify/SimpleBlockingQueueTest.java
@@ -0,0 +1,39 @@
+package waitnotify;
+
+import org.junit.Test;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class SimpleBlockingQueueTest {
+
+    @Test
+    public void whenFetchAllThenGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        Thread producer = new Thread(
+                () -> {
+                    IntStream.range(0, 5).forEach(
+                            queue::offer
+                    );
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        buffer.add(queue.poll());
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer, is(Arrays.asList(0, 1, 2, 3, 4)));
+    }
+}
