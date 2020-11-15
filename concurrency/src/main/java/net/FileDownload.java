2341be3cb1897d62c362de13d8b3c97ff536b838
Ol Gen <oggen18@gmail.com>
2020-11-15 12:28:55 +0300

    Updating Poms for Travis

---
+++ concurrency/src/main/java/net/FileDownload.java
@@ -0,0 +1,23 @@
+package net;
+
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class FileDownload {
+    public static void main(String[] args) throws MalformedURLException {
+        String file = "https://raw.githubusercontent.com/peterarsentev/course_test/master/pom.xml";
+        try (BufferedInputStream in = new BufferedInputStream(new URL(file).openStream());
+             FileOutputStream out = new FileOutputStream("pom_tmp.xml")) {
+            byte[] dataBuffer = new byte[1024];
+            int bytesRead;
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                out.write(dataBuffer, 0, bytesRead);
+                Thread.sleep(1000);
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
