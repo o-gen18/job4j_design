cc9744e16c84def5e052402665a57e53ba71f17c
Ol Gen <oggen18@gmail.com>
2020-11-15 19:27:35 +0300

    EmailNotification corrected

--- concurrency/src/main/java/pool/EmailNotification.java
+++ concurrency/src/main/java/pool/EmailNotification.java
@@ -8,30 +8,8 @@ public class EmailNotification {
     private final ExecutorService pool = Executors.newFixedThreadPool(
             Runtime.getRuntime().availableProcessors());
 
-    public static class User {
-        private String username;
-        private String email;
+    private void send(String subject, String body, String email) {
 
-        public User(String username, String email) {
-            this.username = username;
-            this.email = email;
-        }
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getEmail() {
-            return email;
-        }
-
-        public void setEmail(String email) {
-            this.email = email;
-        }
     }
 
     public void emailTo(User user) {
@@ -41,11 +19,14 @@ public class EmailNotification {
         pool.submit(() -> send(subject, body, user.getEmail()));
     }
 
-    public void send(String subject, String body, String email) {
-
-    }
-
     public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
